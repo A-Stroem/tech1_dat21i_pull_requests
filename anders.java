591ed09a179e9d88d1798dbdc46237b762bdcdce
A-Stroem <asjnyborg@hotmail.com>
2022-03-18 16:01:08 +0100

    Anders hello world file

---
+++ anders.java
@@ -0,0 +1,7 @@
+public class anders {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Second attempt at a pull request");
+    }
+}
